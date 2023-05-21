ec332181df37e59b6cf44cf1b552afb3575a2443
kamil <kamil271e@gmail.com>
2023-05-21 16:14:10 +0200

    [#1] Add more methods to Tensor class

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,31 @@
 #include "tensor.cpp"
 
 int main(){
-    Tensor T(3,3);
+    Tensor T(2,3);
     T.normalDistInit();
     T.display();
+    std::cout<<std::endl;
+    
+    T.transpose();
+    T.display();
+    std::cout<<std::endl;
+    
+    T.flatten();
+    T.display();
+
+    int argmax = T.argmax();
+    std::cout << argmax << std::endl <<std::endl;
+
+    // T.relu();
+    // T.display();
+
+    T.sigmoid();
+    T.display();
+    std::cout<<std::endl;
+
+    T.softmax();
+    T.display();
+    std::cout<<std::endl;
+
     return 0;
 }
--- src/tensor.cpp
+++ src/tensor.cpp
@@ -7,38 +7,133 @@ Tensor::Tensor(int rows, int columns) {
     values.resize(rows, std::vector<double>(columns));
 }
 
+void Tensor::setValue(int i, int j, double val){
+    values[i][j] = val;
+}
+
+double Tensor::getValue(int i, int j){
+    return values[i][j];
+}
+
+int Tensor::getColumns(){
+    return columns;
+}
+
+int Tensor::getRows(){
+    return rows;
+}
+
 void Tensor::normalDistInit(double mean=0.0, double std=1.0) {
     std::random_device rd;
     std::mt19937 gen(rd());
     std::normal_distribution<double> distribution(mean, std);
-    for (int i = 0; i < this->rows; i++) {
-        for (int j = 0; j < this->columns; j++) {
-            this->values[i][j] = distribution(gen);
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            values[i][j] = distribution(gen);
         }
     }
 }
 
 void Tensor::display() {
-    for (int i = 0; i < this->rows; i++) {
+    for (int i = 0; i < rows; i++) {
             std::cout << "[ ";
-            for (int j = 0; j < this->columns; j++) {
-                std::cout << this->values[i][j] << " ";
+            for (int j = 0; j < columns; j++) {
+                std::cout << values[i][j] << " ";
             } std::cout << "]" << std::endl;
         } 
 }
 
 void Tensor::transpose() {
+    std::vector<std::vector<double>> transposed(columns, std::vector<double>(rows));
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            transposed[j][i] = values[i][j];
+        }
+    }
+    std::swap(rows, columns);
+    values = std::move(transposed);
 }
 
-void Tensor::flatten() {
+void Tensor::flatten(int axis=0) {
+    // numpy based: axis=0 -> column vector
+    // axis=1 -> row vector
+    int n = rows * columns;
+    std::vector<std::vector<double>> flattened;
+    if (axis==0){
+        flattened.resize(n, std::vector<double>(1));
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                flattened[i * columns + j][0] = values[i][j];
+            }
+        }
+        rows=n;
+        columns=1;
+    } else if(axis==1){
+        flattened.resize(1, std::vector<double>(n));
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                flattened[0][i * columns + j] = values[i][j];
+            }
+        }
+        rows=1;
+        columns=n;
+    }else{
+        std::cout << "Flatten operation failed. Axis parameter should be 0 or 1." << std::endl;
+        return;
+    }
+    values = std::move(flattened);
 }
 
-int Tensor::argmax() {
-    return -1;
+int Tensor::argmax(int axis=0) {
+    int cur_argmax[2] = {-1,-1};
+    double cur_max = -10000000;
+    for (int i = 0; i < rows; i++){
+        for (int j = 0; j < columns; j++){
+            if (values[i][j] > cur_max){
+                cur_argmax[0] = i;
+                cur_argmax[1] = j;
+                cur_max = values[i][j];
+            }
+        }
+    }
+    if(axis != 0 && axis != 1){
+        std::cout << "Argmax operation failed. Axis parameter should be 0 or 1." << std::endl;
+        return -1;
+    }
+    return cur_argmax[axis];
 }
 
 void Tensor::relu() {
+    for (int i = 0; i < rows; i++){
+        for (int j = 0; j < columns; j++){
+            values[i][j] = (values[i][j] > 0) ? values[i][j] : 0; 
+        }
+    }
 }
 
-void Tensor::softmax() {
+void Tensor::sigmoid(){
+    for (int i = 0; i < rows; i++){
+        for (int j = 0; j < columns; j++){
+            values[i][j] = 1.0 / (1.0 + std::exp(-values[i][j]));
+        }
+    }
+}
+
+void Tensor::softmax(){
+    std::vector<std::vector<double>> probas(rows, std::vector<double>(columns));
+    double sumExp = 0.0;
+
+    for (int i = 0; i < rows; i++){
+        for (int j = 0; j < columns; j++){
+            double expV = std::exp(values[i][j]);
+            probas[i][j] = expV;
+            sumExp += expV;
+        }
+    }
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            probas[i][j] /= sumExp;
+        }
+    }
+    values = std::move(probas);
 }
