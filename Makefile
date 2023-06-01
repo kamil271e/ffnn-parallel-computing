994da7648902d2460fb1835f2c70473dab0f9f1c
kamil <kamil271e@gmail.com>
2023-06-01 21:56:25 +0200

    [#10] Accuracy & execution time plots

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-TRAIN_SET_SIZE := 100 250 500 750 1000
+TRAIN_SET_SIZE := 100 200 300 500 700 800 1000
 
 main:
 	g++ -Wall -o main.out src/main.cpp
@@ -8,8 +8,12 @@ run:
 
 test: # testing duration times
 	mkdir -p times/
+	mkdir -p plots/
 
 	for train_size in $(TRAIN_SET_SIZE); do \
-		./main.out $$train_size 10 30 50 100 200; \
+		./main.out $$train_size 10 30 50 100 150 200; \
 	done
+
+	python3 plotting.py
+
 # numbers of neurons in hidden layer are passed as an argv after train set size
---
+++ plotting.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+import os
+plt.style.use('ggplot')
+
+for file in os.listdir('times'):
+    path = os.path.join('times', file)
+    neurons, times, accuracies = [], [], []
+    train_size = int(file.split('.')[0])
+   
+    with open(path) as f:
+        line = f.readline()
+        while line:
+            neuron, execution_time, accuracy = line.split(' ')
+            neurons.append(int(neuron))
+            times.append(int(execution_time))
+            accuracies.append(float(accuracy))
+            line = f.readline()
+
+        # Time plot
+        plt.plot(neurons, times, '-o')
+        plt.title(f'Time of execution [train size: {train_size}]')
+        plt.xlabel('No. of neurons in hidden layer')
+        plt.ylabel('Time [ms]')
+        plt.savefig(f'plots/time_{train_size}.jpg')
+        plt.clf()
+
+        # Accuracy plot
+        plt.plot(neurons, accuracies, '-o')
+        plt.title(f'Train accuracy [train size: {train_size}]')
+        plt.xlabel('No. of neurons in hidden layer')
+        plt.ylabel('Accuracy')
+        plt.savefig(f'plots/accuracy_{train_size}.jpg')
+        plt.clf()
+
+print('Plots generated and saved.')
